35e497c610d80a44888a10d113177166ad92e26a
Izansax8 <izantorres.alu@iespacomolla.es>
2022-04-26 23:05:57 +0200

    .

--- js/clases/workas.js
+++ js/clases/workas.js
@@ -157,31 +157,7 @@ class Workas extends RotarTurno {
                 responsive: true
             });
         } ); */
-        $(doc).ready(function() {    
-            $('#tablaEmpleados').DataTable({
-                responsive: true,
-                retrieve: true,
-                columnDefs: [
-                    { responsivePriority: 1, targets: 0 },
-                    { responsivePriority: 2, targets: -1 }
-                ],
-                "language": {
-                        "lengthMenu": "Mostrar _MENU_ registros",
-                        "zeroRecords": "No se encontraron resultados",
-                        "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                        "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                        "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-                        "sSearch": "Buscar:",
-                        "oPaginate": {
-                            "sFirst": "Primero",
-                            "sLast":"Último",
-                            "sNext":"Siguiente",
-                            "sPrevious": "Anterior"
-                         },
-                         "sProcessing":"Procesando...",
-                    }
-            });     
-        });
+
 
         this.asignarEvOpBorrarEmpleado();
         this.asignarBtnEvEditarEmpleado();
